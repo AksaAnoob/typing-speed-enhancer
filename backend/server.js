854c2b4d2cda5ddf78847f240f204b16f52c3095
AksaAnoob <aksaanoob@gmail.com>
2026-06-18 13:41:19 +0530

    Added user registration and login system

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const User = require("./models/User");
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(cors());
@@ -50,6 +50,48 @@ app.post("/register", async (req, res) => {
 
         res.status(201).json({
             message: "User registered successfully"
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+});
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Find user
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        // Check password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        // Create token
+        const token = jwt.sign(
+            { userId: user._id },
+            "typemasterysecret",
+            { expiresIn: "7d" }
+        );
+
+        res.json({
+            message: "Login successful",
+            token,
+            username: user.username
         });
 
     } catch (error) {
