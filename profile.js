9febb604ff83966b51fd587698c59c9269b71a59
AksaAnoob <aksaanoob@gmail.com>
2026-06-22 15:16:19 +0530

    updated splash screen

--- profile.js
+++ profile.js
@@ -1,19 +1,58 @@
+let selectedAvatar = null;
+
 const username = localStorage.getItem("username");
 
 document.getElementById("username").innerText =
     username || "Guest";
 
+// load profile data
 fetch(`http://localhost:5000/profile/${username}`)
 .then(res => res.json())
 .then(data => {
 
-    document.getElementById("bestWpm").innerText =
-        data.wpm;
+    document.getElementById("bestWpm").innerText = data.wpm;
+    document.getElementById("bestAccuracy").innerText = data.accuracy + "%";
+    document.getElementById("bestBurst").innerText = data.burstScore;
 
-    document.getElementById("bestAccuracy").innerText =
-        data.accuracy + "%";
+    // load avatar if exists
+    if (data.avatar) {
+        document.getElementById("avatar").src = "avatars/" + data.avatar;
+    }
 
-    document.getElementById("bestBurst").innerText =
-        data.burstScore;
 })
 .catch(err => console.error(err));
+
+/* AVATAR FUNCTIONS */
+
+function openAvatarPicker() {
+    document.getElementById("avatarModal").classList.remove("hidden");
+}
+
+function closeAvatarPicker() {
+    document.getElementById("avatarModal").classList.add("hidden");
+}
+
+function selectAvatar(img) {
+    selectedAvatar = img;
+    document.getElementById("avatar").src = "avatars/" + img;
+}
+
+function saveAvatar() {
+
+    if (!selectedAvatar) return;
+
+    fetch("http://localhost:5000/updateAvatar", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            username: username,
+            avatar: selectedAvatar
+        })
+    })
+    .then(res => res.text())
+    .then(msg => {
+        alert("Avatar updated!");
+        closeAvatarPicker();
+    })
+    .catch(err => console.error(err));
+}
