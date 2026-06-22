442bbc90d7212462b09daa7a21fdcf98f298fa09
AksaAnoob <aksaanoob@gmail.com>
2026-06-22 22:20:39 +0530

    Added avatar field

--- backend/models/user.js
+++ backend/models/user.js
@@ -18,6 +18,11 @@ const userSchema = new mongoose.Schema({
         required: true
     },
 
+    avatar: {
+        type: String,
+        default: "cat"
+    },
+
     createdAt: {
         type: Date,
         default: Date.now
